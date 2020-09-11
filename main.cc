08eb1a79a523ec33cc82b7ef030bc2f51c7f4ab7
soarandfeast <hyenhsiang@gmail.com>
2020-09-11 23:45:42 +0800

    Achieve using stringutil in main.cc
    Now that stringutil can be included in main.cc by simply `#include "stringutil.h" without specify it's actually path.

--- main.cc
+++ main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "stringutil.h"
 
 int add(int a, int b) {
     return a + b;
@@ -6,4 +7,5 @@ int add(int a, int b) {
 
 int main(int argc, char** argv) {
     std::cout << add(1, 2) << std::endl;
+    std::cout << stringutil::is_permutation("abc", "cba") << std::endl;
 }
