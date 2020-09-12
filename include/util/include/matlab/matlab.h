ec514ad12ea80639ec925ea56b69c129c943172d
soarandfeast <hyenhsiang@gmail.com>
2020-09-12 21:24:16 +0800

    Achieve meaningful #include
    Add one more layer of folder in the "include" folder to achieve expressive include statement.
    E.g. `#include "projectname/header.h`.

--- include/util/include/matlab/matlab.h
+++ include/util/include/matlab/matlab.h
@@ -1,7 +1,9 @@
 #pragma once
 #include <vector>
 
-namespace Matlab {
+namespace util {
+namespace matlab {
 std::vector<int> randi(int upper, std::size_t len);
 std::vector<int> randi(int lower, int upper, std::size_t len);
+}
 }
--- include/util/matlab.cc
+++ include/util/matlab.cc
@@ -1,16 +1,16 @@
 #include <random>
-#include "matlab.h"
+#include "include/matlab/matlab.h"
 
 namespace {
 std::random_device seed;  // Standard mersenne_twister_engine seeded with seed()
 std::mt19937 gen(seed());
 }
 
-std::vector<int> Matlab::randi(int upper, size_t len) {
+std::vector<int> util::matlab::randi(int upper, size_t len) {
     return randi(1, upper, len);
 }
 
-std::vector<int> Matlab::randi(int lower, int upper, size_t len) {
+std::vector<int> util::matlab::randi(int lower, int upper, size_t len) {
     std::vector<int> res(len);
     std::uniform_int_distribution<> uniform_int_dist(lower, upper);
     for (auto& n : res) {
